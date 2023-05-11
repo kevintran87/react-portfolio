6ea0abc45d18b09563e0a9312775043f8d9730a5
Kevin Tran <k.tran787@gmail.com>
2023-05-11 16:45:48 -0700

    init

---
+++ src/components/body/about/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./about.css";
+import SocialContact from "../../common/social-contact/index";
+function About() {
+  return (
+    <div className="about">
+      <div className="about-top">
+        <div className="about-info">
+          Hello,
+         <br /> <span className="info-name">I'm Kevin Tran</span>.
+         <br /> I spent the last 10 years in medical sales and am now making a transition into tech. I have a strong passion for web development.
+        </div>
+        <div className="about-photo">
+          <img 
+            src={require("../../../assets/coding.png")}
+            className="picture"
+          />
+        </div>
+      </div>
+      <SocialContact />
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/data/work.js
@@ -0,0 +1,34 @@
+export const WorkData = [
+    {
+        company: "Steris Instrument Management Services" ,
+        designation: "Insights Analyst",
+        dateJoining: "2011",
+        dateEnd: "2022",
+        companyLogo:"https://cdn-icons-png.flaticon.com/512/2537/2537338.png",
+        work:"Meet with customers to understand challenges and present solutions, identify issues before problems occur, develop a process for improvement, and ensure implementation. Lead Proactive Maintenance Inspections for equipment including flexible endoscopes and surgical devices.",
+    },
+    {
+        company: "University of California, Irvine" ,
+        designation: "Full Stack Developer",
+        dateJoining: "10/22",
+        dateEnd: "02/23",
+        companyLogo:"https://cdn-icons-png.flaticon.com/512/2537/2537415.png",
+        work:"HTML/CSS, JavaScript/jQuery, Node.js/Express, React, SQL/NoSQL Databases"
+    },
+    // {
+    //     company: "AESI Sciences Humaines - HeCh" ,
+    //     designation: "W",
+    //     dateJoining: "15.09.2020",
+    //     dateEnd: "15.08.2022",
+    //     companyLogo:"https://cdn-icons-png.flaticon.com/512/2537/2537415.png",
+    //     work:"B2 Enseignant en Sciences Humaines, arrêté pour raisons médicales et reprise de l'activité d'informaticien"
+    // },
+    // {
+    //     company: "Intérim" ,
+    //     designation: "W",
+    //     dateJoining: "2016",
+    //     dateEnd: "Present",
+    //     companyLogo:"https://cdn-icons-png.flaticon.com/512/2537/2537415.png",
+    //     work:"Horéca, Ouvrier de production, Caissier, Technicien Polyvalent en constructions modulaires, Magasinier, Gardien de tournage, Runner Cinéma, Livreur de Matelas,..."
+    // }
+]
